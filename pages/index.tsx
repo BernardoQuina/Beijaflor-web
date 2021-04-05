27405d678c870bf89adfec4e4f40bef4856734a5
Bernardo-Quina <bernardoquina@gmail.com>
2021-04-05 13:45:29 +0100

    Tailwindcss: jit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,10 @@ import { NextPage } from 'next'
 interface HomeProps {}
 
 const Home: NextPage<HomeProps> = () => {
-
   return (
-    <div>Hello world!</div>
+    <div className='min-h-screen bg-blue-500 text-blue-50 flex items-center justify-center'>
+      Hello world!
+    </div>
   )
 }
 
