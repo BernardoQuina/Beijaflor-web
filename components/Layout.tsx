1020b90a911262d16644d4eb9c4311273439bb96
Bernardo-Quina <bernardoquina@gmail.com>
2021-04-07 18:48:26 +0100

    Better animate shared layout for nav

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,8 +8,8 @@ interface LayoutProps {
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className='relative z-[0] w-screen h-screen overflow-x-hidden bg-green-extraLight scrollbar-thin scrollbar-thumb-pink-dark scrollbar-thumb-rounded-full'>
-      <div className='absolute z-[-1] ml-[100%] md:ml-[80%] 1xl:ml-[70%] 2xl:ml-[60%] 3xl:ml-[50%]'>
-        <div className='h-[100rem] w-[100rem] rounded-[6rem] shadow-inner-2 transform rotate-45 bg-pink-light'></div>
+      <div className='absolute z-[-1] ml-[140%] md:ml-[100%] lg:-ml[80%] 1xl:ml-[70%] 2xl:ml-[60%] 3xl:ml-[50%]'>
+        <div className='h-[180rem] w-[180rem] rounded-[4rem] shadow-inner-2 transform rotate-45 bg-pink-light'></div>
       </div>
       <Navbar />
       <main className='w-full 3xl:w-[90%] 4xl:w-[70%] mt-36 mx-auto px-3'>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,45 +9,28 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
 
   return (
     <AnimateSharedLayout>
-      {!open ? (
-        <motion.nav
-          layoutId='expandable-nav'
-          className='fixed w-[90%] 4xl:w-[70%] h-[5rem] mt-3 mx-auto left-0 right-0 rounded-3xl shadow-lg bg-white'
-        >
-          <div className='relative w-full h-full'>
-            <motion.button
-              type='button'
-              layoutId='expandable-nav-button'
-              className='absolute bottom-0 mx-auto left-0 right-0 bg-gray-400 rounded-full focus:outline-none'
-              onClick={() => setOpen(!open)}
-            >
-              <ExpandMoreRounded
-                fontSize='large'
-                className='text-gray-400 bg-white transform'
-              />
-            </motion.button>
-          </div>
-        </motion.nav>
-      ) : (
-        <motion.nav
-          layoutId='expandable-nav'
-          className='fixed w-[90%] 4xl:w-[70%] h-[30rem] mt-3 mx-auto left-0 right-0 rounded-3xl shadow-lg bg-white'
-        >
-          <div className='relative w-full h-full'>
-            <motion.button
-              type='button'
-              layoutId='expandable-nav-button'
-              className='absolute bottom-0 mx-auto left-0 right-0 bg-gray-400 rounded-full focus:outline-none'
-              onClick={() => setOpen(!open)}
-            >
-              <ExpandMoreRounded
-                fontSize='large'
-                className='text-gray-400 bg-white transform rotate-180'
-              />
-            </motion.button>
-          </div>
-        </motion.nav>
-      )}
+      <motion.nav
+        layoutId='expandable-nav'
+        className={`fixed w-[95%] 3xl:w-[85%] 4xl:w-[70%] ${
+          !open ? ' h-[4.5rem]' : ' h-[30rem]'
+        } mt-5 mx-auto left-0 right-0 rounded-xl shadow-lg bg-white`}
+      >
+        <div className='relative w-full h-full'>
+          <motion.button
+            type='button'
+            layoutId='expandable-nav-button'
+            className='absolute bottom-0 mx-auto left-0 right-0 bg-gray-400 rounded-full focus:outline-none'
+            onClick={() => setOpen(!open)}
+          >
+            <ExpandMoreRounded
+              fontSize='large'
+              className={`text-gray-400 bg-white transform ${
+                open && 'rotate-180'
+              }`}
+            />
+          </motion.button>
+        </div>
+      </motion.nav>
     </AnimateSharedLayout>
   )
 }
