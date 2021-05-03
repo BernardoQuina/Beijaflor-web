c5a1818243dc8f19579d938dbeb4c14772bef43a
Bernardo-Quina <bernardoquina@gmail.com>
2021-05-03 08:57:25 +0100

    useIsAuth and isServer utils & resize login page presentation section

--- pages/login.tsx
+++ pages/login.tsx
@@ -10,6 +10,7 @@ import { Blob } from '../components/svg/Blob'
 import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion'
 import { MeDocument, MeQuery, useLoginMutation } from '../lib/generated/graphql'
 import { InputField } from '../components/InputField'
+import { useIsAuth } from '../utils/useIsAuth'
 
 interface loginProps {}
 
@@ -20,6 +21,8 @@ const login: NextPage<loginProps> = ({}) => {
 
   const [login] = useLoginMutation({ errorPolicy: 'all' })
 
+  useIsAuth()
+
   return (
     <Layout>
       <div className='-mt-4 mb-48 min-h-[40rem] w-full max-w-7xl mx-auto grid grid-cols-1 grid-rows-2 lg:grid-rows-1 lg:grid-cols-2'>
@@ -28,19 +31,19 @@ const login: NextPage<loginProps> = ({}) => {
             tailwind='mt-6 lg:mt-0 text-pink-dark h-[2.8rem] xs:h-[4rem] lg:h-[5rem] mx-auto'
             strokeWidth={2.5}
           />
-          <Blob tailwind='absolute -left-6 lg:-left-4 lg:bottom-0 w-[110%] lg:w-[40rem] z-[-1] text-pink-light' />
+          <Blob tailwind='absolute lg:-top-8 -left-6 lg:left-[50%] transform lg:translate-x-[-52%]  w-[110%] lg:w-[35rem] z-[-1] text-pink-light' />
           <Blob
-            tailwind='absolute -left-6 lg:-left-4 lg:bottom-0 w-[110%] lg:w-[40rem] z-[-2] text-green-light'
+            tailwind='absolute lg:-top-8 -left-6 lg:left-[50%] transform lg:translate-x-[-52%] w-[110%] lg:w-[35rem] z-[-2] text-green-light'
             rotate={160}
           />
-          <h1 className='mt-2 lg:mt-12 text-sm xs:text-base lg:text-4xl text-center font-thin tracking-[0.3rem] xs:tracking-[0.5rem] text-pink-dark'>
+          <h1 className='mt-2 lg:mt-12 text-sm xs:text-base lg:text-3xl text-center font-thin tracking-[0.3rem] md:tracking-[0.5rem] text-pink-dark'>
             FLORISTA BEIJAFLOR
           </h1>
-          <div className='flex flex-col xs:flex-row mt-2 xs:mt-10 lg:mt-24 mx-auto'>
-            <h2 className='xs:mr-4 font-bold text-center lg:text-4xl text-pink-dark'>
+          <div className='flex flex-col xs:flex-row mt-2 xs:mt-10 lg:mt-20 mx-auto'>
+            <h2 className='xs:mr-4 font-bold text-center lg:text-3xl text-pink-dark'>
               20
             </h2>
-            <h3 className='text-pink-dark text-center text-sm xs:text-base lg:text-2xl self-center font-thin tracking-widest w-[10rem] xs:w-max'>
+            <h3 className='text-pink-dark text-center text-sm xs:text-base lg:text-xl self-center font-thin tracking-widest w-[10rem] xs:w-max'>
               anos de momentos especiais
             </h3>
           </div>
@@ -49,7 +52,7 @@ const login: NextPage<loginProps> = ({}) => {
           </h4>
           <Link href='/explorar'>
             <a className='m-auto mt-5 xs:mt-14 lg:mt-20'>
-              <button className='p-2 px-2 rounded-md shadow-md text-white opacity-95 bg-opacity-80 hover:opacity-100 hover:bg-opacity-100 bg-green-medium font-thin tracking-widest text-sm xs:text-base md:text-2xl '>
+              <button className='p-1 lg:p-2 px-2 rounded-md shadow-md text-white opacity-95 bg-opacity-80 hover:opacity-100 hover:bg-opacity-100 bg-green-medium font-thin tracking-widest text-sm xs:text-base md:text-xl '>
                 EXPLORAR
               </button>
             </a>
---
+++ utils/useIsAuth.ts
@@ -0,0 +1,25 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useMeQuery } from '../lib/generated/graphql'
+import { isServer } from './isServer'
+
+export const useIsAuth = (push?: boolean) => {
+  const router = useRouter()
+
+  const { data, loading } = useMeQuery({ errorPolicy: 'all', skip: isServer() })
+
+  useEffect(() => {
+    if (!loading && !data?.me && push) {
+      router.push('/login')
+    }
+
+    if (
+      data?.me &&
+      (router.pathname === '/login' ||
+        router.pathname === '/register' ||
+        router.pathname.includes('change-password'))
+    ) {
+      router.push('/')
+    }
+  }, [loading, data, push])
+}
