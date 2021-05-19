6def765f6bccc025573cae886a2004b231cae811
Bernardo-Quina <bernardoquina@gmail.com>
2021-05-19 12:12:23 +0100

    createCartItem from productItem component card

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -3,7 +3,13 @@ import { Image } from 'cloudinary-react'
 
 import { Heart } from './svg/Heart'
 import { ShoppingBag } from './svg/ShoppingBag'
-import { BasicProductInfoFragment } from '../lib/generated/graphql'
+import {
+  BasicProductInfoFragment,
+  useCreateCartItemMutation,
+  useMeQuery,
+} from '../lib/generated/graphql'
+import { isServer } from '../utils/isServer'
+import { useCartModal } from '../context/CartModalContext'
 
 interface ProductItemProps {
   product: BasicProductInfoFragment
@@ -19,6 +25,12 @@ export const ProductItem: React.FC<ProductItemProps> = ({
   sm,
 }) => {
   const routeName = encodeURIComponent(product.name).replace(/%20/g, '-')
+
+  const { setCartModal } = useCartModal()
+
+  const { data } = useMeQuery({ errorPolicy: 'all', skip: isServer() })
+
+  const [createCartItem] = useCreateCartItemMutation({ errorPolicy: 'all' })
 
   return (
     <div
@@ -45,7 +57,29 @@ export const ProductItem: React.FC<ProductItemProps> = ({
             <h6 className='ml-4 tracking-widest leading-tight text-pink-medium'>
               {product.categories[product.categories.length - 1].name}
             </h6>
-            <button className='absolute bottom-0 right-1 rounded-full p-2 bg-opacity-20 bg-white hover:bg-opacity-100 hover:scale-110'>
+            <button
+              className='absolute bottom-0 right-1 rounded-full p-2 bg-opacity-20 bg-white hover:bg-opacity-100 hover:scale-110'
+              type='button'
+              onClick={async (e) => {
+                e.preventDefault()
+
+                if (data && data.me !== null) {
+                  await createCartItem({
+                    variables: {
+                      cartId: data.me.cart.id,
+                      productId: product.id,
+                      quantity: 1,
+                    },
+                    update: (cache) => {
+                      cache.evict({ fieldName: 'CartItems' })
+                      cache.evict({ id: `Cart:${data.me.cart.id}` })
+                    },
+                  })
+
+                  setCartModal('true')
+                }
+              }}
+            >
               <ShoppingBag tailwind='h-8 text-pink-dark' strokeWidth={1.8} />
             </button>
             <h4
