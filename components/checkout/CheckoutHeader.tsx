5f46944081a7d052ecb79d31cbfe8fd66a8ac8a2
Bernardo-Quina <bernardoquina@gmail.com>
2021-05-28 12:15:30 +0100

    ConfirmItems component end

--- components/checkout/CheckoutHeader.tsx
+++ components/checkout/CheckoutHeader.tsx
@@ -1,3 +1,5 @@
+import { motion, AnimateSharedLayout } from 'framer-motion'
+
 import { ArrowDown } from '../svg/ArrowDown'
 
 interface CheckoutHeaderProps {
@@ -8,102 +10,119 @@ export const CheckoutHeader: React.FC<CheckoutHeaderProps> = ({
   checkoutFase,
 }) => {
   return (
-    <div className='flex mx-auto -mt-10 max-w-4xl'>
-      <h1
-        className={`${
-          checkoutFase !== 'confirm items' && 'hidden md:inline-block'
-        }
-            relative z-[0] mt-1 lg:mt-4 mx-auto font-serif text-xl md:text-3xl tracking-widest text-pink-dark ${
-              checkoutFase === 'confirm items'
-                ? 'text-xl md:text-3xl'
-                : 'text-sm md:text-xl font-thin self-end'
-            }`}
-      >
-        Cesto
-        {checkoutFase === 'confirm items' && (
-          <div className='absolute z-[-1] ml-1 -mt-3 rounded-sm bg-pink-light w-full h-[0.4rem]'></div>
-        )}
-      </h1>
-      <div
-        className={`${
-          checkoutFase !== 'confirm items' && 'hidden md:inline-block'
-        } self-end`}
-      >
-        <ArrowDown
-          tailwind='h-6 text-pink-dark transform -rotate-90'
-          strokeWidth={2}
-        />
-      </div>
-      <h1
-        className={`${
-          checkoutFase !== 'confirm items' &&
-          checkoutFase !== 'address' &&
-          'hidden md:inline-block'
-        }
-            relative z-[0] mt-1 lg:mt-4 mx-auto font-serif text-xl md:text-3xl tracking-widest text-pink-dark ${
-              checkoutFase === 'address'
-                ? 'text-xl md:text-3xl'
-                : 'text-sm md:text-xl font-thin self-end'
-            }`}
-      >
-        Morada
-        {checkoutFase === 'address' && (
-          <div className='absolute z-[-1] ml-1 -mt-3 rounded-sm bg-pink-light w-full h-[0.4rem]'></div>
-        )}
-      </h1>
-      <div
-        className={`${
-          checkoutFase !== 'address' && 'hidden md:inline-block'
-        } self-end`}
-      >
-        <ArrowDown
-          tailwind='h-6 text-pink-dark transform -rotate-90'
-          strokeWidth={2}
-        />
-      </div>
-      <h1
-        className={`${
-          checkoutFase !== 'address' &&
-          checkoutFase !== 'payment' &&
-          'hidden md:inline-block'
-        }
-            relative z-[0] mt-1 lg:mt-4 mx-auto font-serif text-xl md:text-3xl tracking-widest text-pink-dark ${
-              checkoutFase === 'payment'
-                ? 'text-xl md:text-3xl'
-                : 'text-sm md:text-xl font-thin self-end'
-            }`}
-      >
-        Pagamento
-        {checkoutFase === 'payment' && (
-          <div className='absolute z-[-1] ml-1 -mt-3 rounded-sm bg-pink-light w-full h-[0.4rem]'></div>
-        )}
-      </h1>
-      <div
-        className={`${
-          checkoutFase !== 'payment' && 'hidden md:inline-block'
-        } self-end`}
-      >
-        <ArrowDown
-          tailwind='h-6 text-pink-dark transform -rotate-90'
-          strokeWidth={2}
-        />
-      </div>
-      <h1
-        className={`${
-          checkoutFase !== 'payment' &&
-          checkoutFase !== 'confirmation' &&
-          'hidden md:inline-block'
-        } relative z-[0] mt-1 lg:mt-4 mx-auto font-serif text-xl md:text-3xl tracking-widest text-pink-dark ${
-          checkoutFase === 'confirmation'
+    <div className='flex mx-auto -mt-12 lg:-mt-14 max-w-4xl'>
+      <AnimateSharedLayout>
+        <motion.h1
+          layoutId='confirm Items'
+          className={`${
+            checkoutFase !== 'confirm items' && 'hidden md:inline-block'
+          }
+        relative z-[0] mt-1 lg:mt-4 mx-auto font-serif text-xl md:text-3xl tracking-widest text-pink-dark ${
+          checkoutFase === 'confirm items'
             ? 'text-xl md:text-3xl'
             : 'text-sm md:text-xl font-thin self-end'
         }`}
-      >
-        Confirmação
-        {checkoutFase === 'confirmation' && (
-          <div className='absolute z-[-1] ml-1 -mt-3 rounded-sm bg-pink-light w-full h-[0.4rem]'></div>
-        )}
-      </h1>
+        >
+          Cesto
+          {checkoutFase === 'confirm items' && (
+            <motion.div
+              layoutId='checkout-underline'
+              className='absolute z-[-1] ml-1 -mt-3 rounded-sm bg-pink-light w-full h-[0.4rem]'
+            ></motion.div>
+          )}
+        </motion.h1>
+        <div
+          className={`${
+            checkoutFase !== 'confirm items' && 'hidden md:inline-block'
+          } self-end`}
+        >
+          <ArrowDown
+            tailwind='h-6 text-pink-dark transform -rotate-90'
+            strokeWidth={2}
+          />
+        </div>
+        <motion.h1
+          layoutId='address'
+          className={`${
+            checkoutFase !== 'confirm items' &&
+            checkoutFase !== 'address' &&
+            'hidden md:inline-block'
+          }
+        relative z-[0] mt-1 lg:mt-4 mx-auto font-serif text-xl md:text-3xl tracking-widest text-pink-dark ${
+          checkoutFase === 'address'
+            ? 'text-xl md:text-3xl'
+            : 'text-sm md:text-xl font-thin self-end'
+        }`}
+        >
+          Morada
+          {checkoutFase === 'address' && (
+            <motion.div
+              layoutId='checkout-underline'
+              className='absolute z-[-1] ml-1 -mt-3 rounded-sm bg-pink-light w-full h-[0.4rem]'
+            ></motion.div>
+          )}
+        </motion.h1>
+        <div
+          className={`${
+            checkoutFase !== 'address' && 'hidden md:inline-block'
+          } self-end`}
+        >
+          <ArrowDown
+            tailwind='h-6 text-pink-dark transform -rotate-90'
+            strokeWidth={2}
+          />
+        </div>
+        <motion.h1
+          layoutId='payment'
+          className={`${
+            checkoutFase !== 'address' &&
+            checkoutFase !== 'payment' &&
+            'hidden md:inline-block'
+          }
+        relative z-[0] mt-1 lg:mt-4 mx-auto font-serif text-xl md:text-3xl tracking-widest text-pink-dark ${
+          checkoutFase === 'payment'
+            ? 'text-xl md:text-3xl'
+            : 'text-sm md:text-xl font-thin self-end'
+        }`}
+        >
+          Pagamento
+          {checkoutFase === 'payment' && (
+            <motion.div
+              layoutId='checkout-underline'
+              className='absolute z-[-1] ml-1 -mt-3 rounded-sm bg-pink-light w-full h-[0.4rem]'
+            ></motion.div>
+          )}
+        </motion.h1>
+        <div
+          className={`${
+            checkoutFase !== 'payment' && 'hidden md:inline-block'
+          } self-end`}
+        >
+          <ArrowDown
+            tailwind='h-6 text-pink-dark transform -rotate-90'
+            strokeWidth={2}
+          />
+        </div>
+        <h1
+          className={`${
+            checkoutFase !== 'payment' &&
+            checkoutFase !== 'confirmation' &&
+            'hidden md:inline-block'
+          } relative z-[0] mt-1 lg:mt-4 mx-auto font-serif text-xl md:text-3xl tracking-widest text-pink-dark ${
+            checkoutFase === 'confirmation'
+              ? 'text-xl md:text-3xl'
+              : 'text-sm md:text-xl font-thin self-end'
+          }`}
+        >
+          Confirmação
+          {checkoutFase === 'confirmation' && (
+            <motion.div
+              layoutId='checkout-underline'
+              className='absolute z-[-1] ml-1 -mt-3 rounded-sm bg-pink-light w-full h-[0.4rem]'
+            ></motion.div>
+          )}
+        </h1>
+      </AnimateSharedLayout>
     </div>
   )
 }
--- components/checkout/ConfirmItems.tsx
+++ components/checkout/ConfirmItems.tsx
@@ -1,17 +1,211 @@
 import { Dispatch, SetStateAction } from 'react'
-import { MeQuery } from '../../lib/generated/graphql'
-import { CartContent } from '../navbar/CartContent'
+import Link from 'next/link'
+import { Image } from 'cloudinary-react'
 
+import {
+  MeQuery,
+  useChangeItemQuantityMutation,
+  useRemoveItemMutation,
+} from '../../lib/generated/graphql'
+import { X } from '../svg/X'
+import { Plus } from '../svg/Plus'
+import { Minus } from '../svg/Minus'
 
 interface ConfirmItemsProps {
   data: MeQuery
   setCheckoutFase: Dispatch<SetStateAction<string>>
 }
 
-export const ConfirmItems: React.FC<ConfirmItemsProps> = ({ data, setCheckoutFase }) => {
+export const ConfirmItems: React.FC<ConfirmItemsProps> = ({
+  data,
+  setCheckoutFase,
+}) => {
+  const [changeItemQuantity] = useChangeItemQuantityMutation({
+    errorPolicy: 'all',
+  })
+
+  const [removeItem] = useRemoveItemMutation({ errorPolicy: 'all' })
+
   return (
-    <div className='mt-8 flex flex-col mx-auto max-w-6xl bg-white rounded-md shadow-around'>
-      <CartContent cart={data.me.cart} data={data} /> !!! mudar !!!
+    <div className='mt-3 xs:mt-6 flex flex-col mx-auto max-w-2xl h-[27rem] xs:h-[40rem] bg-white rounded-md shadow-around'>
+      {data?.me?.cart === null || data?.me?.cart.cartItems.length < 1 ? (
+        <h4 className='m-auto tracking-wider text-green-dark'>
+          O seu cesto está vazio! Adicione algo para continuar.
+        </h4>
+      ) : (
+        <div className='flex flex-col w-full'>
+          <div className='w-full flex h-[2rem] border-b'>
+            <div className='flex w-[80%]'>
+              <h3 className='mt-1 ml-3 w-full font-bold text-green-dark tracking-wide'>
+                Produtos
+              </h3>
+            </div>
+            <div className='hidden lg:flex w-[20%] ml-auto'>
+              <h3 className='mt-1 text-center w-full font-bold text-green-dark tracking-wide'>
+                Subtotal
+              </h3>
+            </div>
+          </div>
+          <div className='flex flex-col w-full pb-4 px-3 lg:px-4 h-[21rem] xs:h-[34rem] overflow-y-scroll scrollbar-thin scrollbar-thumb-green-light scrollbar-thumb-rounded-full'>
+            {data?.me?.cart.cartItems.map((cartItem) => (
+              <div className='flex w-full' key={cartItem.id}>
+                <div className='flex relative mx-auto w-[99%] lg:w-[80%] p-2 mt-3 h-[5.4rem] rounded-md shadow-around'>
+                  <div className='flex'>
+                    <div className='w-14 h-16 m-auto flex overflow-hidden rounded-md'>
+                      <Image
+                        className='my-auto'
+                        cloudName={
+                          process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
+                        }
+                        publicId={cartItem.product.images[0]}
+                        quality={20}
+                        height={250}
+                        width={200}
+                        gravity='auto'
+                        crop='fill'
+                        secure={true}
+                      />
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[70%]'>
+                    <div className='flex flex-col ml-4 my-auto'>
+                      <Link
+                        href={`/produtos/${encodeURIComponent(
+                          cartItem.product.name
+                        ).replace(/%20/g, '-')}`}
+                      >
+                        <a>
+                          <h5 className='mt-auto leading-tight text-green-dark font-serif tracking-wider'>
+                            {cartItem.product.name}
+                          </h5>
+                        </a>
+                      </Link>
+                      <div className='mb-auto flex mt-1'>
+                        <h5 className='mr-1 text-xs text-green-dark font-bold'>
+                          €
+                        </h5>
+                        <h5 className='text-green-dark font-bold'>
+                          {cartItem.product.price.toFixed(2)}
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                  <button
+                    className='absolute right-2 top-2 flex'
+                    type='button'
+                    onClick={async () => {
+                      if (data?.me) {
+                        await removeItem({
+                          variables: {
+                            cartItemId: cartItem.id,
+                            cartId: data.me.cart.id,
+                            productId: cartItem.product.id,
+                          },
+                          update: (cache) => {
+                            cache.evict({ id: `CartItem:${cartItem.id}` })
+                            cache.evict({ id: `Cart:${data.me.cart.id}` })
+                          },
+                        })
+                      }
+                    }}
+                  >
+                    <X tailwind='h-4 text-green-medium' />
+                  </button>
+                  <div className='flex absolute m-auto w-[20%] right-2 bottom-2'>
+                    <button
+                      className='h-5 self-center'
+                      onClick={async () => {
+                        if (data?.me) {
+                          await changeItemQuantity({
+                            variables: {
+                              plusOrMinusOne: -1,
+                              cartItemId: cartItem.id,
+                              cartId: data.me.cart.id,
+                              productId: cartItem.product.id,
+                            },
+                            update: (cache) => {
+                              cache.evict({ id: `CartItem:${cartItem.id}` })
+                              cache.evict({ id: `Cart:${data.me.cart.id}` })
+                            },
+                          })
+                        }
+                      }}
+                      disabled={cartItem.quantity <= 1}
+                    >
+                      <Minus tailwind='h-4 text-green-dark' strokeWidth={2} />
+                    </button>
+                    <p className='self-center mx-auto font-bold text-green-dark'>
+                      {cartItem.quantity}
+                    </p>
+                    <button
+                      className='h-5 self-center'
+                      onClick={async () => {
+                        if (data?.me) {
+                          await changeItemQuantity({
+                            variables: {
+                              plusOrMinusOne: 1,
+                              cartItemId: cartItem.id,
+                              cartId: data.me.cart.id,
+                              productId: cartItem.product.id,
+                            },
+                            update: (cache) => {
+                              cache.evict({ id: `CartItem:${cartItem.id}` })
+                              cache.evict({ id: `Cart:${data.me.cart.id}` })
+                            },
+                          })
+                        }
+                      }}
+                      disabled={cartItem.quantity >= cartItem.product.stock}
+                    >
+                      <Plus tailwind='h-4 text-green-dark' strokeWidth={2} />
+                    </button>
+                  </div>
+                </div>
+                <div className='hidden lg:flex lg:flex-col lg:w-[20%]'>
+                  <div className='m-auto flex'>
+                    <h5 className='mr-1 text-xs text-green-dark font-bold'>
+                      €
+                    </h5>
+                    <h5 className='text-green-dark font-bold'>
+                      {(cartItem.product.price * cartItem.quantity).toFixed(2)}
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className='flex w-full h-[4rem] border-t'>
+            <div className='flex flex-col w-[55%] lg:w-[78%] p-2'>
+              <div className='flex'>
+                <h3 className='text-green-dark tracking-wide'>TOTAL</h3>
+                <h3 className='ml-auto mr-1 font-bold text-sm text-green-dark'>
+                  €
+                </h3>
+                <h3 className='font-bold text-green-dark'>
+                  {data?.me.cart.price.toFixed(2)}
+                </h3>
+              </div>
+              <div className='flex'>
+                <h3 className='text-green-dark tracking-wide'>PRODUTOS</h3>
+                <h3 className='ml-auto font-bold text-green-dark'>
+                  {data?.me.cart.quantity}
+                </h3>
+              </div>
+            </div>
+            <div className='flex w-[45%] lg:w-[22%]  ml-auto p-2'>
+              <button
+                type='button'
+                className='h-full w-full max-w-[8rem] ml-auto flex flex-col rounded-md shadow-md bg-green-extraLight'
+                onClick={() => setCheckoutFase('address')}
+              >
+                <h5 className='text-center m-auto font-bold text-green-dark tracking-wider'>
+                  Continuar
+                </h5>
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
